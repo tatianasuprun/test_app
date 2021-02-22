fe8738a644a763b3e2271bb009c062a015b619a9
Tatiana Suprun <suprun.tata@gmail.com>
2021-02-22 23:42:19 +0200

    added crawler for products; implemented loading products from one selected category

---
+++ lib/crawlers/product_crawler.rb
@@ -0,0 +1,74 @@
+class ProductCrawler < Mechanize
+  HOMEPAGE_URL = 'https://www.tohome.com'.freeze
+  CATEGORY_PRODUCTS_URL = 'https://www.tohome.com/catalog.aspx?catalog_id=736&catalog_name=Computer-Accessories'
+
+  def initialize(*args)
+    super(args)
+    set_cookies
+  end
+
+  def pagination(category_link = '')
+    document = page_document(CATEGORY_PRODUCTS_URL)
+
+    pages_selector = document.css('div.CatalogPageLink > span#lblPageLink').first
+    pages_selector_content = pages_selector.children
+
+    page_products(category_link = '')
+    unless pages_selector_content.empty?
+      for i in 1..(pages_number(document) - 1)
+        pages_selector = document.css('div.CatalogPageLink > span#lblPageLink').first
+        pages_selector_content = pages_selector.children.last
+        next_page_link = pages_selector_content['href']
+        document = page_document(next_page_link)
+        page_products(next_page_link)
+      end
+    end
+  end
+
+  private
+
+  def prepare_string(items)
+    after_needed = items.index(' items')
+    l = items.size
+    items.slice!(after_needed, l - 1)
+    before_needed = items.index('of ')
+    items.slice!(0, before_needed + 3)
+    items.to_i
+  end
+
+  def pages_number(document)
+    items_info = document.css('div#list > header > div.pageNo > span#lblCount').first.children.first.text
+    total_items = prepare_string(items_info)
+    total_items / 30 + (total_items % 30 ? 1 : 0)
+  end
+
+  def page_products(category_link = '')
+    page_document(category_link).css('span#lblProductDesc > div.mainbox').each do |div|
+      a = div.css('a').first
+      product_name = div.css('div.prdInfo > h2.prdTitle').first.children.first.text
+      product_price = div.css('div.prdInfo > span.prdPrice-new').first.children.first.text
+      product_url = a['href']
+      category = Category.find_by_url(CATEGORY_PRODUCTS_URL)
+      begin
+        current_product = Product.create({ name: product_name, url: product_url, price: product_price })
+      ensure
+        current_product = Product.find_by_url(product_url)
+      end
+      category.products << current_product unless category.products.include?(current_product)
+    end
+  end
+
+  def page_document(link)
+    page = get(link)
+    Nokogiri::HTML(page.body)
+  rescue StandardError => e
+    abort("Error occured: #{e.message}")
+  end
+
+  def set_cookies
+    cookie = Mechanize::Cookie.new('langCookie', 'langCookie=en-us')
+    cookie.domain = '.tohome.com'
+    cookie.path = '/'
+    cookie_jar << cookie
+  end
+end
--- lib/tasks/crawler.rake
+++ lib/tasks/crawler.rake
@@ -1,9 +1,17 @@
-require_relative '../ crawlers/category_crawler'
+require_relative '../crawlers/category_crawler'
+require_relative '../crawlers/product_crawler'
 
 namespace :crawl do
   desc 'Fetch categories'
   task categories: :environment do
     agent = CategoryCrawler.new
     agent.run
+  end
+
+  desc 'Fetch products'
+  task products: :environment do
+    agent = ProductCrawler.new
+
+    agent.pagination
   end
 end
